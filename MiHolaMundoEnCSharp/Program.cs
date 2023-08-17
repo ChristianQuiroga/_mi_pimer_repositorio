8f2fcebdab59398ed5eb92e45049aab0f6b12832
ChristianQuiroga <142511529+ChristianQuiroga@users.noreply.github.com>
2023-08-17 12:59:08 -0300

    primer PRJ C#

---
+++ MiHolaMundoEnCSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MiHolaMundoEnCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
